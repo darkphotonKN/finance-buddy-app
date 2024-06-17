05b702e503828fa10de2423cca9db39630fb4213
Kranti <darkphoton20@gmail.com>
2024-06-18 03:29:17 +0800

    Initial Commit

---
+++ FinBud/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinBud
+//
+//  Created by Kranti on 2024/6/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinBud/FinBudApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinBudApp.swift
+//  FinBud
+//
+//  Created by Kranti on 2024/6/18.
+//
+
+import SwiftUI
+
+@main
+struct FinBudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
